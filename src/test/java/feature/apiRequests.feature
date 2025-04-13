ff5806c2e7838c7e65fe9fa16e59d88f1612cec8
nivasmukte <nivasmukte81@gmail.com>
2025-04-14 00:30:48 +0530

    added post request

--- src/test/java/feature/apiRequests.feature
+++ src/test/java/feature/apiRequests.feature
@@ -3,3 +3,12 @@ Feature: Test all the request of an API
     Given verify get request endpoint
     When Send the get request
     Then verify the status code
+
+  Scenario: Verify post endpoint of API
+    Given verify post request endpoint
+    When send the post request
+      | title  | foo |
+      | body   | bar |
+      | userId | 1   |
+    Then verify the status code for post request
+
--- src/test/java/stepDefinition/stepDefinition.java
+++ src/test/java/stepDefinition/stepDefinition.java
@@ -1,12 +1,17 @@
 package stepDefinition;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.json.JSONObject;
 import org.junit.Assert;
 import utility.configReader;
+
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
@@ -14,6 +19,7 @@ public class stepDefinition {
 
     private String endpoint;
     private Response response;
+    private JSONObject requestJson;
 
     @Given("verify get request endpoint")
     public void verify_get_request_endpoint() {
@@ -32,4 +38,37 @@ public class stepDefinition {
 
     }
 
+    @Given("verify post request endpoint")
+    public void verifyPostRequestEndpoint() {
+        String baseURL=configReader.getProperty("baseUrl");
+        this.endpoint=baseURL+"/posts";
+        
+    }
+
+    @When("send the post request")
+    public void sendThePostRequest(DataTable dataTable) {
+        Map<String, String> data = dataTable.asMap(String.class, String.class);
+        requestJson = new JSONObject();
+        for (Map.Entry<String, String> entry : data.entrySet()) {
+            // Try parsing value as integer if it's numeric
+            String key = entry.getKey();
+            String value = entry.getValue();
+            try {
+                int intValue = Integer.parseInt(value);
+                requestJson.put(key, intValue);
+            } catch (NumberFormatException e) {
+                requestJson.put(key, value);
+            }
+            response = given()
+                    .contentType(ContentType.JSON)
+                    .body(requestJson.toString())
+                    .when()
+                    .post(endpoint);
+        }
+    }
+
+    @Then("verify the status code for post request")
+    public void verifyTheStatusCodeForPostRequest() {
+        System.out.println(response.getStatusCode());
+    }
 }
